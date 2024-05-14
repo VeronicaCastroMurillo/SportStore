b069e2c39908e33370767a54c81270d8a96295ea
VeronicaCastroMurillo <veronicacastromurillo@gmail.com>
2024-05-13 21:11:06 -0600

    Correcciones y creacion de Http Client

---
+++ src/services/products.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private http: HttpClient) { }
+
+ generateProducts(): Observable<Response>{
+     return this.http.get<Response>('http://localhost:5000/getProductsNames');
+ }
+
+}
--- src/views/login/login.component.ts
+++ src/views/login/login.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatError } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../services/products.service';
+import { catchError } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormField, MatError, ReactiveFormsModule, MatInputModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+ loginForm!: FormGroup;
+ 
+  
+ constructor(private formBuilder: FormBuilder, public productsService: ProductsService){
+  this.loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+ }
+ 
+ ngOnInit(): void {
+
+ }
+
+onSubmit (){
+   if(this.loginForm.valid){
+     alert(this.loginForm.value)
+   }
+   else{
+    this.loginForm.markAllAsTouched();
+   }
+}
+
+generateAllProducts(): void {
+
+  this.productsService.generateProducts().subscribe({
+    next: (data) => {
+      console.log(data)
+    },
+    error: (e) => {console.log(e)}
+  })
+}
 
 }
